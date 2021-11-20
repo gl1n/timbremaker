21c749b40d71e89e743a743f6eebebaa49b1a22b
lu <1769982399@qq.com>
2021-11-20 11:38:56 +0800

    config the clipp lib

---
+++ build/src/CMakeFiles/timbre_maker.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lu/Code/cpp/timbre_maker/src/int2chars.cpp" "/home/lu/Code/cpp/timbre_maker/build/src/CMakeFiles/timbre_maker.dir/int2chars.cpp.o"
+  "/home/lu/Code/cpp/timbre_maker/src/sin.cpp" "/home/lu/Code/cpp/timbre_maker/build/src/CMakeFiles/timbre_maker.dir/sin.cpp.o"
+  "/home/lu/Code/cpp/timbre_maker/src/wave.cpp" "/home/lu/Code/cpp/timbre_maker/build/src/CMakeFiles/timbre_maker.dir/wave.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/timbre_maker.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libtimbre_maker.a"
+)
